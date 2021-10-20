f43dce9f442cb6de67c2abbbf8dc9cc06c3c9c04
laumenghui <laumenghui@gmail.com>
2021-10-20 10:59:40 +0800

    配置VS code 运行dotnet 程序 思考离开IDE之后的工作流和对工具的掌握

---
+++ C#.NET Demo/AsyncAwait/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // for (int counter = 1; counter < 5; counter++)
+            // {
+            //     if (counter % 3 == 0)
+            //     {
+            //         WriteFactorialAsyncUsingTask(counter);
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine(counter);
+            //     }
+            // }
+
+            await DoSomething(1);
+            DoOtherSomething(2).Wait();
+            
+            // Console.ReadLine();
+
+        }
+
+        private static async Task DoSomething(int i)
+        {
+            Console.WriteLine("先运行");
+            await Task.Delay(2000);
+            Console.WriteLine(DateTime.Now.ToString() + "----" + i.ToString());
+        }
+
+        private static async Task DoOtherSomething(int i)
+        {
+            await Task.Delay(100);
+            Console.WriteLine("先运行");
+            Task.Delay(3000);
+            Console.WriteLine(DateTime.Now.ToString() + "----" + i.ToString());
+        }
+
+        private static void WriteFactorialAsyncUsingTask(int no)
+        {
+            Task<int> task=Task.Run<int>(() =>
+            {
+                int result = FindFactorialWithSimulatedDelay(no);
+                return result;
+            });
+            task.ContinueWith(new Action<Task<int>>((input) =>
+            {
+                Console.WriteLine("Factorial of {0} is {1}", no, input.Result);
+            }));
+        }
+
+        private static int FindFactorialWithSimulatedDelay(int no)
+        {
+            int result = 1;
+            for (int i = 1; i <= no; i++)
+            {
+                Thread.Sleep(500);
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
