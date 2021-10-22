e8c59713ef70058b0152e1c7f7e5c8c98af4856d
laumenghui <laumenghui@gmail.com>
2021-10-22 16:23:35 +0800

    CEF 浏览器嵌入 WPF和WInfrom 调用开源chrome 或者调用Webview Edge 浏览器作为小操作系统 基本可以覆盖很多功能 浏览网页 解析JS PDF操作等等

---
+++ C#.NET Demo/CefSharpDemo/CefSharpDemo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CefSharpDemo
+{
+	 /// <cefSharp:ChromiumWebBrowser Grid.Row="1"
+     /// Address="{Binding BrowserAddress, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
+     /// Title="{Binding BrowserTitle, Mode=OneWay, UpdateSourceTrigger=PropertyChanged}"
+     /// Name="browser" />
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Cef.Initialize(new Settings { LogSeverity = LogSeverity.Disable, PackLoadingDisabled = true });
+
+            InitializeComponent();
+
+            //browser.Address = "http://bing.com";
+
+            browser.Address = @"E:\文档\软件文档\agilent-86100a_manual.pdf";
+
+
+            ///浏览器的通知事件
+            browser.StatusMessage += BrowserStatusMessage;
+            browser.NavStateChanged += BrowserNavStateChanged;
+
+        }
+
+
+        // https://www.codeproject.com/Articles/887148/Display-HTML-in-WPF-and-CefSharp-Tutorial-Part-2
+    }
+}
