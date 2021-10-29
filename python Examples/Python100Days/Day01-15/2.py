dbe05735202f8881f04f36ea540508cba22a5642
laumenghui <laumenghui@gmail.com>
2021-10-29 15:52:21 +0800

    第三四五天练习 针对语言基础语法进行练习 这些个东西联想 C C++ C# 回顾语言的发展历史 从语法到语义 要知道深层原理 体系和层次

--- python Examples/Python100Days/Day01-15/2.py
+++ python Examples/Python100Days/Day01-15/2.py
@@ -39,6 +39,7 @@ print(s)
 
 
 import math
+from random import randint
 perimeter = 8 * 2 * math.pi
 print(perimeter)
 
@@ -52,4 +53,48 @@ print(isLeapYear(2100))
 print(isLeapYear(2020))
 print(isLeapYear(2021))
 
+lst = [i+1 for i in range(100)]
+print(sum(lst))
 
+i =1
+sum_add = 0 
+# for i in range(101):
+#     sum_add +=i
+#     i+=1
+# print(sum_add)
+
+while i <= 100:
+    sum_add += i
+    i+=1
+print(sum_add)
+
+sum_0 = 0
+for i in range(101):
+    sum_0 += i
+print(sum_0)
+
+
+# import random
+
+# # ran = int(random.random() * 100)
+# ran = random.randint(0, 100)
+# count = 0
+# while True:
+#     num = int(input("请输入要猜的整数{0-100}"))
+#     count +=1
+#     if ran == num:
+#         print("猜中了")
+#         break
+#     elif num < ran:
+#         print("猜的小了点")
+#     else:
+#         print("猜的大了点")
+
+# print("一共猜了%d次" % count)
+
+#九九乘法表
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print("%d * %d = %d" % (i, j, i * j), end='\t')
+    #换行
+    print()
