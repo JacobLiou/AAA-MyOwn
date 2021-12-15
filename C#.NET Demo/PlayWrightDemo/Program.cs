40c68d77c9663b615c7f5ab581ab84a649f7e007
liumenghui <liumenghui@genomics.cn>
2021-12-15 10:13:55 +0800

    微软新的WEB浏览器自动化测试框架 替代selenium

---
+++ C#.NET Demo/PlayWrightDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Playwright;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using var playwright = await Playwright.CreateAsync();
+// playwright.Chromium.ExecutablePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+await using var browser = await playwright.Chromium.LaunchAsync();
+// var browser = await playwright.Chromium.LaunchAsync();
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://github.com");
+await page.ScreenshotAsync(new PageScreenshotOptions{ Path = "screenshot.png"});
--- python Examples/DSAlgo.py
+++ python Examples/DSAlgo.py
@@ -2,4 +2,4 @@
 a = [1, 4, 5, 9, 3, 1]
 a.sort()
 
-pass
+print(a)
