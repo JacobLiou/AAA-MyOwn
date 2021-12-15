8fa7ae0da4a8d3f8608091ca17d755135dadbc05
liumenghui <liumenghui@genomics.cn>
2021-12-15 10:19:23 +0800

    针对新的不熟悉语法进行了解6W1H

--- C#.NET Demo/PlayWrightDemo/Program.cs
+++ C#.NET Demo/PlayWrightDemo/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Hello, World!");
 
 using var playwright = await Playwright.CreateAsync();
 // playwright.Chromium.ExecutablePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+//await using C# 8语法 针对实现了IAsyncDisposable接口 异步去清理资源 提高并发
 await using var browser = await playwright.Chromium.LaunchAsync();
 // var browser = await playwright.Chromium.LaunchAsync();
 var page = await browser.NewPageAsync();
